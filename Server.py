8a87740949968f35b4df876cc67156c612176a65
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2022-11-10 08:38:46 -0800

    this is the updated code for automation ping

--- Server.py
+++ Server.py
@@ -1,10 +1,28 @@
+import os
+import subprocess
+os.system("ping -n 18.246.215.35 ")
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
         # TODO -
         self.server_ip = server_ip
+        self.ping()
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        try:
+            # Use subprocess to run the ping command
+            result = subprocess.run(["ping", "-n", "4", self.server_ip], capture_output=True, text=True, check=True)
+
+            # Check if the ping was successful
+            if "0% packet loss" in result.stdout:
+                return True
+            else:
+                return False
+
+        except subprocess.CalledProcessError:
+            # An error occurred (e.g., invalid IP address, network unreachable)
+            return False
+
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-# This is the template code for the CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
-# CNA 337 Fall 2020
-
+# This is the template code for the CNE335 Final Project
+# Daothidtern
+# CNE 335 Fall
+import server
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by daothid")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
     # TODO - Call Ping method and print the results
+    ec2_server_ip ="18.246.215.35"
+    ec2_server = server.Server(ec2_server_ip)
+
+    if ec2_server.ping():
+        print(f"The server at {ec2_server_ip} is reachable.")
+    else:
+        print(f"The server at {ec2_server_ip} is not reachable.")
