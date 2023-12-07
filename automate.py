c55d0f2dad03c5a3ca0281b28578469574363c7e
Daothid tern <terndao@gmail.com>
2023-12-06 22:10:08 -0800

    this is the frist code for automate ssh final code

--- automate.py
+++ automate.py
@@ -1,14 +1,13 @@
 #Daothi tern
 #Automate ssh
-import paramiko
 
 import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # Use double backslashes or a raw string for the file path
-ssh.connect(hostname='18.246.215.35', username='ubuntu', key_filename=r'C:\Users\thidd\PycharmProjects_automation\your_private_key.pem')
 
+ssh.connect(hostname='34.209.241.162', username='ubuntu', key_filename=r'C:\Users\thidd\PycharmProjects_automation\keypair.pem')
 
 
 # Run update command
@@ -17,7 +16,7 @@ stdin, stdout, stderr = ssh.exec_command(update_command)
 
 # Print the output of the update command
 print("Update Command Output:")
-for line in stdout.read().splitlines():
+for line in stdout.read().decode().splitlines():
     print(line)
 
 # Run upgrade command
@@ -25,7 +24,6 @@ upgrade_command = 'sudo apt-get upgrade -y'
 stdin, stdout, stderr = ssh.exec_command(upgrade_command)
 
 # Print the output of the upgrade command
-print("\nUpgrade Command Output:")
 for line in stdout.read().splitlines():
     print(line)
 
